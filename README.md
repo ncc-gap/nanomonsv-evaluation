272d9f6e2ebd08d3cfb5681d6d6a09df8994826e
aokad <aokada@ncc.go.jp>
2024-04-08 13:32:39 +0900

    New create

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # nanomonsv-evaluation
+
+Evaluation tools for [nanomonsv](https://github.com/friend1ws/nanomonsv.git).
+
+benchmark scripts
+
+ - https://github.com/ncc-gap/nanomonsv_benchmark.git
+   A part of https://github.com/friend1ws/nanomonsv_paper.git
---
+++ nanomonsv/tests/check_download.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+import os
+
+def check_download(url, output_path):
+    
+    import requests
+
+    if os.path.dirname(output_path) != "" and not os.path.exists(os.path.dirname(output_path)):
+       os.makedirs(os.path.dirname(output_path))
+
+    if not os.path.exists(output_path):
+        response = requests.get(url)
+        with open(output_path, 'wb') as hout:
+            hout.write(response.content)
+
+def check_download_s3(bucket_name, object_name, file_name):
+
+    import boto3
+    if not os.path.exists(os.path.dirname(file_name)):
+        os.makedirs(os.path.dirname(file_name))
+
+    if not os.path.exists(file_name):
+        s3 = boto3.client('s3')
+        s3.download_file(bucket_name, object_name, file_name)
+
